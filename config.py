1b71ea1183affdf309f794c93a2c6ce3fd6c6dfe
Atrix-Zero <66100751+Atrix-Zero@users.noreply.github.com>
2020-08-01 15:30:00 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+
+account = {
+	"Username":"NewCuenta5",
+	"Password":"xp10it-sistem"
+}
+
+
+bet = {
+	"Basebet":"0.000001",
+	"Chance1":"53",
+	"Chance2":"53"
+}
+
+
+
+betset = {
+	"IFLose":"2.65",
+	"IFWin":"2",
+	"TargetProfit":"10",
+	"TargetLose":"1000000",
+	"ResetBetIfWin":"1",
+	"ResetBetIfLose":"0",
+	"ResetSeedEachBet":"ON"
+}
+
+
+withdraw = {
+	"AutoWithdraw":"OFF",
+	"AmountWithdraw":"0",
+	"WithdrawIfBalance":"0",
+	"Address":""
+}
